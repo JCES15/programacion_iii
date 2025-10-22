95d97116409e723c520be1ff4c71a1da04e7463b
JC Endara <jendara@elsauce.edu.ec>
2025-10-22 08:23:57 -0500

    clase 2025-10-21

---
+++ modulo_2/javascript/02_variables.js
@@ -0,0 +1,8 @@
+console.log("variables");
+var saludo1 = "HOLA";
+var saludo2 ="hola desde la variable";
+console.log(saludo1);
+
+console.log(saludo1+saludo2);
+console.log(saludo1+"  "+saludo2);
+
